cc9c7be1800066f2c999ff0bed4078129e922b50
li xie <xie00053@algonquinlive.com>
2019-06-14 10:50:14 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-button type="primary" @click="openDiag = true">Welcome</el-button>
+    <el-dialog :visible.sync="openDiag">
+      <HelloWorld msg="My frist App" />
+      <!-- 添加props步骤1 v-blind和设定名称，然后在data里设定初始值，-->
+    <div>
+      <input type="text" v-model="sendTreat">
+    </div>
+    <!-- // 添加component步骤4: 在 template 里添加 -->
+    <!-- 添加props步骤2 v-blind和设定名称，然后在data里设定初始值，-->
+    <Lixie :treat="sendTreat"/>
+    </el-dialog> 
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-
+// 步骤1: 在components文件夹里新建文件，文件名第一个字母必须大写，如LiFristVue.vue。
+// 步骤2: 在 script 里 import 文件，可以定义为任何名称,如：import Lixie from "./components/LiFristVue.vue";。
+import Lixie from "./components/LiFristVue.vue";
+// 步骤3: 在 export default 的 components里添加，名称与import的名称一致
 export default {
   name: "app",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Lixie
+  },
+  data: function() {
+    return {
+      // 添加props 步骤3:设定data, 然后在child component里接收
+      sendTreat: "Raw Hide",
+      openDiag: false,
+    };
+  },
 };
 </script>
 
---
+++ src/components/LiFristVue.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h2>Hello Li!</h2>
+        <ul>
+            <li>Age: {{ age }}</li>
+            <li>Color: {{ color }}</li>
+            <!-- 添加props 步骤5: 将props值显示在浏览器 -->
+            <li>{{ treat }}</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    // 添加props 步骤4: 接受从parent component里的 props 值
+    props: ["treat"],
+    data: function() {
+        return {
+          name: "Li",
+          age: 6,
+          color: "Black"
+        };
+    }
+};
+</script>
+
+<style lang="scss">
+h2 {
+    color: coral;
+};
+// $color: coral;
+// h2 {
+//     color: $coral;
+// }
+
+
+</style>
+
+
